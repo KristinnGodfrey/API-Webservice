8c0a6004ab0f7975f9d7a4df3562e458dc3be3b3
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-03-16 22:12:26 +0000

    feat: cloudinary files

---
+++ src/setup.js
@@ -0,0 +1,28 @@
+import { uploadImagesFromDisk } from "../sql/images.js";
+
+const {
+  DATABASE_URL: databaseUrl,
+  CLOUDINARY_CLOUD = cloudinaryUrl,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,  
+  IMAGE_FOLDER: imageFolder = "./data/img",
+} = process.env;
+
+async function main() {
+  console.info(`Set upp gagnagrunn á ${databaseUrl}`);
+  console.info(`Set uppp tengingu við Cloudinary á ${cloudinaryUrl}`);
+  
+  // senda myndir á Cloudinary
+  try {
+    images = await uploadImagesFromDisk(imageFolder);
+    console.info(`Sendi ${images.length} myndir á Cloudinary`);
+  } catch (e) {
+    console.error("Villa við senda myndir á Cloudinary:", e.message);
+  }
+}
+
+main().catch((err) => {
+    console.error(err);
+});
+  
+
