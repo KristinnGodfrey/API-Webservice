7af1a91b2eab2658b20f970e92830e79f5f2e2a0
Fridrikhr <Fridrikhr96@gmail.com>
2021-03-17 11:00:18 +0000

    app keyrist upp

--- src/app.js
+++ src/app.js
@@ -1,19 +1,51 @@
 import express from 'express';
 import { router as tvRouter }  from './tv.js';
 import dotenv from 'dotenv';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import session from 'express-session';
+
+import { router as authenticateRouter} from './authenticate.js';
+import passport from './login.js';
 
 dotenv.config();
 
 const {
     PORT: port = 3000,
+    SESSION_SECRET: sessionSecret,
 } = process.env;
 
+if (!sessionSecret) {
+    console.error('Vantar gögn í env');
+    process.exit(1);
+  }
+
 const app = express();
+
+const path = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(path, '../public')));
+
+
+app.set('views', join(path, '../views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.json('Hello World!')
 })
 
+app.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    maxAge: 20 * 1000, // 20 sek
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/users', authenticateRouter);
 app.use('/tv', tvRouter);
 // app.use('/tv/:id/season', seasonRouter);
 // app.use('/tv/:id/season/:seasonId/episode', episodeRouter)
